5a96c8959c3229cafd858e43407a124d3f1b31a2
Katie Fenn <katie.fenn@googlemail.com>
2016-11-14 14:05:20 +0000

    111: Memcached nonce storage
    - Added nonce-checker middleware
    - Installed Memcached package
    - Some configuration changes

--- test/unit/src/server/handlers/t_login.js
+++ test/unit/src/server/handlers/t_login.js
@@ -1,5 +1,6 @@
 import expect from 'expect';
-import { spy, stub } from 'sinon';
+import { spy, stub, match } from 'sinon';
+import proxyquire from 'proxyquire';
 import {
   logout,
   errorHandler
@@ -12,13 +13,16 @@ describe('login', () => {
     req = {
       session: {
         destroy: stub().callsArg(0)
+      },
+      body: {
+        'openid.response_nonce': '2016-11-15T10:05:11ZYDVKpj'
       }
     };
     res = {
       send: spy(),
       redirect: spy()
-    },
-      next = spy();
+    };
+    next = spy();
   });
 
   describe('logout handler', () => {
@@ -51,4 +55,93 @@ describe('login', () => {
     });
 
   });
+
+  describe('nonce checker handler', () => {
+    let memcachedNonceChecker;
+
+    context('when Memcached service is available', () => {
+      let add;
+      context('and nonce exists', () => {
+
+        beforeEach(() => {
+          add = stub().callsArgWith(3, true);
+          memcachedNonceChecker = proxyquireHelper({
+            'memcached': memcachedMockHelper(add),
+            '../configure.js': {
+              get: stub().withArgs('SESSION_MEMCACHED_HOST').returns('127.0.0.1:11211')
+            }
+          });
+          memcachedNonceChecker(req, res, next);
+        });
+
+        it('should add nonce to Memcached storage with 5 minutes lifetime', () => {
+          expect(add.calledWith(
+            'OPENID_NONCE:11ZYDVKpj',
+            '2016-11-15T10:05:11ZYDVKpj',
+            5 * 60,
+            match.func
+          )).toBe(true);
+        });
+
+        it('should call next middleware with an error', () => {
+          expect(next.calledWith(match.instanceOf(Error))).toBe(true);
+        });
+      });
+
+      context('and nonce does not exist', () => {
+        beforeEach(() => {
+          add = stub().callsArgWith(3, false);
+          memcachedNonceChecker = proxyquireHelper({
+            'memcached': memcachedMockHelper(add),
+            '../configure.js': {
+              get: stub().withArgs('SESSION_MEMCACHED_HOST').returns('127.0.0.1:11211')
+            }
+          });
+          memcachedNonceChecker(req, res, next);
+        });
+
+        it('should add nonce to Memcached storage with 5 minutes lifetime', () => {
+          expect(add.calledWith(
+            'OPENID_NONCE:11ZYDVKpj',
+            '2016-11-15T10:05:11ZYDVKpj',
+            5 * 60,
+            match.func
+          )).toBe(true);
+        });
+
+        it('should call next middleware with no error', () => {
+          expect(next.calledWithExactly()).toBe(true);
+        });
+      });
+
+    });
+
+    context('when Memcached service is not available', () => {
+      beforeEach(() => {
+        memcachedNonceChecker = proxyquireHelper({
+          '../configure.js': {
+            get: stub().withArgs('SESSION_MEMCACHED_HOST').returns(null)
+          }
+        });
+        memcachedNonceChecker(req, res, next);
+      });
+
+      it('should call next middleware with no error', () => {
+        expect(next.calledWithExactly()).toBe(true);
+      });
+    });
+  });
 });
+
+function memcachedMockHelper(addStub) {
+  return function () {
+    this.add = addStub;
+  };
+}
+
+function proxyquireHelper(options) {
+  return proxyquire(
+    '../../../../../src/server/handlers/login.js',
+    options
+  ).memcachedNonceChecker;
+}
