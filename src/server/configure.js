da3d4b570f699cae2a402d322fdd62b078051193
Katie Fenn <katie.fenn@googlemail.com>
2016-11-21 14:58:19 +0000

    117: Config overhaul
    - Added path resolution to env file loading
    - Re-arranged loading of arguments and env file

--- src/server/configure.js
+++ src/server/configure.js
@@ -1,11 +1,13 @@
+const path = require('path');
 const nconf = require('nconf');
 
-let envFile = 'environments/development.env';
-if (nconf.get('env')) {
-  envFile = nconf.get('env');
-}
-
 nconf.argv();
+
+let envFile = 'environments/development.env';
+
+if (nconf.get('env')) {
+  envFile = path.resolve(__dirname, '../..', nconf.get('env'));
+}
 
 require('dotenv').config({ path: envFile });
 
