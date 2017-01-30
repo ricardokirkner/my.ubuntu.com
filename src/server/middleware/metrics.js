6c311046c9bbc1604d82ce75cbb05529e8babacb
Stephen <stephen.stewart@carisenda.com>
2017-01-30 13:39:03 +0000

    no need for statsd string here (#250)
    * no need for statsd string here
    
    * fix tests for statsd hierarcy string
    
    * set scope correctly
    
    * create instance once, not per request
    
    * clearer indentation

--- src/server/middleware/metrics.js
+++ src/server/middleware/metrics.js
@@ -4,13 +4,30 @@ import responseTime from 'response-time';
 import conf from '../configure';
 import logging from '../logging';
 
+const unit = conf.get('JUJU_UNIT');
+const stage = conf.get('SERVICE_ENVIRONMENT');
+const statsdDsn = conf.get('STATSD_DSN');
+const logger = logging.getLogger('express');
+const metrics = createStatsdClient(statsdDsn, 'ols.', e => logger.debug(e));
+
+export function responseMetricsMiddleware(req, res, time) {
+
+  if (!statsdDsn) {
+    logger.debug('STATSD_DSN not set, reponseTime metrics cannot be reported.');
+    return;
+  }
+
+  const stat = createStatName(stage, unit, req, res);
+
+  logger.info(stat, time);
+  metrics.timing(stat, time);
+}
+
 export function createStatName(stage='devel', unit, req, res) {
   const { url, method } = req;
   const { statusCode } = res;
 
   let stat = [
-    'statsd',
-    'ols',
     stage,
     'mu',
     unit,
@@ -26,28 +43,18 @@ export function createStatName(stage='devel', unit, req, res) {
   return stat.filter(n => n).join('.');
 }
 
-export function responseMetricsMiddleware(req, res, time) {
-  const statsdDsn = conf.get('STATSD_DSN');
-  const logger = logging.getLogger('express');
-
-  if (!statsdDsn) {
-    logger.debug('STATSD_DSN not set, no metrics for you!');
+export function createStatsdClient(dsn, scope='', errorHandler) {
+  if (!dsn) {
+    logger.debug('createStatsdClient, missing required param dsn.');
     return;
   }
+  const { hostname, port } = url.parse(dsn);
 
-  const { hostname, port, path } = url.parse(statsdDsn);
-  const metrics = new lynx(hostname, port, {
-    scope: path,
-    on_error: (err) => {
-      logger.debug(err);
-    }
+  return new lynx(hostname, port, {
+    scope: scope,
+    on_error: errorHandler
   });
-  const unit = conf.get('JUJU_UNIT');
-  const stage = conf.get('SERVICE_ENVIRONMENT');
-  const stat = createStatName(stage, unit, req, res);
-
-  logger.info(stat, time);
-  metrics.timing(stat, time);
 }
+
 
 export default responseTime(responseMetricsMiddleware);
--- test/unit/src/server/middleware/t_metrics.js
+++ test/unit/src/server/middleware/t_metrics.js
@@ -1,11 +1,14 @@
 import expect from 'expect';
+import lynx from 'lynx';
 
 import {
+  createStatsdClient,
   createStatName
 } from '../../../../../src/server/middleware/metrics';
 
 describe('metrics middleware', function() {
 
+  let client;
   let req;
   let res;
 
@@ -17,23 +20,42 @@ describe('metrics middleware', function() {
     res = {
       statusCode: 200
     };
+
+    client = createStatsdClient('udp://localhost:1234', 'ols.');
+  });
+
+  describe('createStatsdClient', function() {
+
+    it('should create a lynx instance', function() {
+      expect(client).toBeA(lynx);
+    });
+
+    it('should have correct scope', function() {
+      expect(client.scope).toBe('ols.');
+    });
+
   });
 
   describe('createStatName', function() {
 
     it('should create a stat name', function() {
       expect(createStatName('staging', 'unit', req, res))
-        .toBe('statsd.ols.staging.mu.unit.views.foo_bar.GET.200.response');
+        .toBe('staging.mu.unit.views.foo_bar.GET.200.response');
     });
 
     it('should filter missing name parts', function() {
       expect(createStatName('staging', null, req, res))
-        .toBe('statsd.ols.staging.mu.views.foo_bar.GET.200.response');
+        .toBe('staging.mu.views.foo_bar.GET.200.response');
     });
 
     it('should sanitise urls', function() {
       expect(createStatName('staging', null, req, res))
-        .toBe('statsd.ols.staging.mu.views.foo_bar.GET.200.response');
+        .toBe('staging.mu.views.foo_bar.GET.200.response');
+    });
+
+    it('should default to devel', function() {
+      expect(createStatName(undefined, undefined, req, res))
+        .toBe('devel.mu.views.foo_bar.GET.200.response');
     });
   });
 });
